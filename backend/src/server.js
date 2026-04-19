703c3d1d7c184192cec0abcececb4da10f848fe9
matos17igor <igorm17_pvp@hotmail.com>
2026-04-18 23:45:41 -0300

    feat: adiciona cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(PORT, () => {
